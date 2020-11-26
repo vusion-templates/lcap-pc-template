3b59fa024f36e7c027c932b1f142574e120d2c1e
Forrest <rainforest92@126.com>
2020-11-26 21:43:25 +0800

    :sparkles: Add api.yaml

--- scripts/pc/config.js
+++ scripts/pc/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: {
         name: 'cloud-admin-template',
-        version: '0.14.20',
+        version: '0.14.21',
     },
     server: {
         name: 'cloud-admin-fullstack',
