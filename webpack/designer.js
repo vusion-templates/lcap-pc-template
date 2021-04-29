7f08d4a1276a23df57d7e5f5cd7ec91402b31985
Forrest <rainforest92@126.com>
2021-04-29 10:36:20 +0800

    :bookmark: Publish 0.3.0

--- webpack/designer.js
+++ webpack/designer.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 module.exports = {
     config(baseConfig, pages) {
-        baseConfig.outputDir = (baseConfig.outputDir || 'pkg');
+        baseConfig.outputDir = (baseConfig.outputDir || 'public');
         fs.emptyDirSync(path.resolve(baseConfig.outputDir));
         Object.keys(pages).forEach((pageName) => {
             delete pages[pageName];
