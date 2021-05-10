66e15b8c7c33e9f1bd275ff4c4c01e8486d5294b
Forrest <rainforest92@126.com>
2021-05-10 17:46:32 +0800

    修改错误文案

--- src/global/features/service/create/errHandles.js
+++ src/global/features/service/create/errHandles.js
@@ -2,27 +2,27 @@ import { UToast } from 'cloud-ui.vusion';
 export default {
     defaults({ config }, err) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统繁忙');
+            UToast.show(err.msg || '系统错误，请联系管理员！');
         }
     },
     500({ config }, err = {}) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统繁忙');
+            UToast.show(err.msg || '系统错误，请联系管理员！');
         }
     },
     400({ config }, err = {}) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统繁忙');
+            UToast.show(err.msg || '系统错误，请联系管理员！');
         }
     },
     remoteError({ config }, err) {
         if (!config.noErrorTip) {
-            UToast.show('请求失败');
+            UToast.show('系统错误，请联系管理员！');
         }
     },
     localError({ config }, err) {
         if (!config.noErrorTip) {
-            UToast.show('客户端错误');
+            UToast.show('系统错误，请联系管理员！');
         }
     },
 };
