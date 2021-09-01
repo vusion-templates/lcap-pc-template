eb6a3332dec2adb282c3de4063bfb91fc3fb4abc
violarong <xiaorong_hz@163.com>
2021-09-01 13:47:48 +0800

    fix Bug-16420 接口错误提示增加Message

--- src/global/features/service/create/errHandles.js
+++ src/global/features/service/create/errHandles.js
@@ -2,17 +2,17 @@ import { UToast } from 'cloud-ui.vusion';
 export default {
     defaults({ config }, err) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统错误，请联系管理员！');
+            UToast.show(err.Message || err.msg || '系统错误，请联系管理员！');
         }
     },
     500({ config }, err = {}) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统错误，请联系管理员！');
+            UToast.show(err.Message || err.msg || '系统错误，请联系管理员！');
         }
     },
     400({ config }, err = {}) {
         if (!config.noErrorTip) {
-            UToast.show(err.msg || '系统错误，请联系管理员！');
+            UToast.show(err.Message || err.msg || '系统错误，请联系管理员！');
         }
     },
     remoteError({ config }, err) {
