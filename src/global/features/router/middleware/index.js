b4c10347234f9a54160eac14808845151fc60c98
daysai <wddpwzz36@gmail.com>
2021-09-22 16:55:19 +0800

    :bug: fix redirect index page

--- src/global/features/router/middleware/index.js
+++ src/global/features/router/middleware/index.js
@@ -1,8 +1,7 @@
 import { getComponentOption } from '@/global/middleware/util';
 import resolveAsyncComponents from './resolve-async-components';
 import middlewareHandleList from '@/global/middleware';
-const runMiddleWare = function (middlewareList, context) {
-    let p = Promise.resolve();
+const runMiddleWare = async function (middlewareList, context) {
     let called = false;
     const next = context.next;
     context.next = function (...args) {
@@ -14,43 +13,36 @@ const runMiddleWare = function (middlewareList, context) {
             next(...args);
         }
     };
-    while (middlewareList.length) {
-        const middleware = middlewareList.shift();
-        p = p.then(() => {
+    try {
+        for (const middleware of middlewareList) {
             if (called) {
-                return Promise.reject();
+                throw new Error(`${middleware} jumped! next is called`);
             } else if (middlewareHandleList[middleware]?.before) {
-                const out = middlewareHandleList[middleware].before(context);
-                if (out && out.then) {
-                    return out;
+                // eslint-disable-next-line no-await-in-loop
+                const out = await middlewareHandleList[middleware].before(context);
+                if (out) {
+                    console.warn(`${middleware} finished`, out);
+                } else if (called) {
+                    throw new Error(`${middleware} finished without return, next is called`);
                 } else {
-                    return called ? Promise.reject() : Promise.resolve();
+                    console.warn(`${middleware} finished without return, next is not called`);
                 }
             } else {
                 console.error(`${middleware} not exist`);
-                return Promise.resolve();
             }
-        }, (e) => {
-            console.error(`${middleware} run error`, e);
-            middlewareList = [];
-            if (!called) {
-                called = true;
-            }
-            return Promise.reject();
-        });
-    }
-    p.then(() => {
+        }
         if (!called) {
+            // eslint-disable-next-line require-atomic-updates
             called = true;
             next();
         }
-    }, (e) => {
-        console.log(e);
+    } catch (error) {
         if (!called) {
+            // eslint-disable-next-line require-atomic-updates
             called = true;
             next(false);
         }
-    });
+    }
 };
 export const beforeMiddleware = function (router, appConfig) {
     // 提前加载异步组件
