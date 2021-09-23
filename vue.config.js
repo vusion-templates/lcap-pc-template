d37baa892fae99ca6cd5450ecda3130c96329a3c
daysai <wddpwzz36@gmail.com>
2021-09-23 10:37:14 +0800

    :fire: rm useless micro code

--- vue.config.js
+++ vue.config.js
@@ -8,23 +8,12 @@ if (argv.pages) {
             delete pages[key];
     });
 }
-if (Object.keys(pages).length > 1) {
-    Object.keys(pages).forEach((key) => {
-        if (pages[key].micro) {
-            console.warn(`page[${key}] is microApp`);
-            delete pages[key];
-        }
-    });
-}
 const isDevelopment = process.env.NODE_ENV === 'development';
 const publicPathPrefix = process.env.SITE_TYPE === 'gh-pages' ? `https://vusion-templates.github.io/${pkg.name}` : '/';
 
 const port = argv.port || 8810;
 
 const devServer = require('./webpack.dev-server')(port);
-
-const webpackMicro = require('./webpack/micro');
-const isMicro = webpackMicro.isMicro(pages);
 
 const webpackDll = require('./webpack/dll');
 const webpackCloudUI = require('./webpack/cloud-ui');
@@ -36,11 +25,8 @@ const webpackGQL = require('./webpack/gqloader');
 const webpackOptimization = require('./webpack/optimization');
 const isDesigner = process.env.BUILD_LIB_ENV === 'designer';
 
-if (isMicro) {
-    webpackMicro.setup(pages);
-}
 const assetsDir = 'public';
-let baseConfig = {
+const baseConfig = {
     publicPath: publicPathPrefix,
     assetsDir,
     productionSourceMap: false,
@@ -51,9 +37,6 @@ let baseConfig = {
         /@cloud-ui/,
     ],
 };
-if (isMicro) {
-    baseConfig = webpackMicro.config(baseConfig, port, isDevelopment);
-}
 
 if (isDesigner) {
     webpackDesigner.config(baseConfig, pages);
@@ -66,11 +49,7 @@ const vueConfig = {
             webpackDesigner.chain(config, pages);
         } else {
             webpackHtml.chain(config, isDevelopment);
-            if (isMicro) {
-                webpackMicro.chain(config, isDevelopment);
-            } else {
-                webpackDll.chain(config, publicPathPrefix, isDevelopment);
-            }
+            webpackDll.chain(config, publicPathPrefix, isDevelopment);
         }
         webpackOptimization.chain(config, isDevelopment);
 
@@ -78,7 +57,7 @@ const vueConfig = {
         webpackStyle.chain(config);
         webpackRoutes.chain(config);
         webpackGQL.chain(config);
-        config.output.jsonpFunction('webpackJsonp' + pkg.name + (isMicro ? Object.keys(pages)[0] : ''));
+        config.output.jsonpFunction('webpackJsonp' + pkg.name);
 
         config.module.rule('js').uses.delete('cache-loader');
     },
