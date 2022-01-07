d8c8082b21d064114d2cad6073d826851d8dcf89
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2022-01-07 09:56:00 +0800

    feat: support hasUserCenter

--- src/global/features/router/auth/index.js
+++ src/global/features/router/auth/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import authService from '@/global/services/auth';
+import lowauthService from '@/global/services/lowauth';
 import cookie from '@/global/features/utils/cookie';
 
 let userInfoPromise = null;
@@ -11,19 +12,35 @@ const getBaseHeaders = () => ({
 });
 
 const request = function (times) {
-    return authService.GetUser({
-        headers: getBaseHeaders(),
-        config: {
-            noErrorTip: true,
-        },
-    }).then((result) => result.Data).catch((err) => {
-        times--;
-        if (times > 0) {
-            return request(times);
-        } else {
-            throw err;
-        }
-    });
+    if (window.appInfo.hasUserCenter) {
+        return lowauthService.GetUser({
+            headers: getBaseHeaders(),
+            config: {
+                noErrorTip: true,
+            },
+        }).then((result) => result).catch((err) => {
+            times--;
+            if (times > 0) {
+                return request(times);
+            } else {
+                throw err;
+            }
+        });
+    } else {
+        return authService.GetUser({
+            headers: getBaseHeaders(),
+            config: {
+                noErrorTip: true,
+            },
+        }).then((result) => result.Data).catch((err) => {
+            times--;
+            if (times > 0) {
+                return request(times);
+            } else {
+                throw err;
+            }
+        });
+    }
 };
 const auth = {
     _map: undefined,
@@ -31,6 +48,13 @@ const auth = {
         if (!userInfoPromise) {
             userInfoPromise = request(times).then((userInfo) => {
                 const $global = Vue.prototype.$global = Vue.prototype.$global || {};
+                if (window.appInfo.hasUserCenter) {
+                    // 格式转化
+                    userInfo = {
+                        UserName: userInfo?.userName,
+                        UserId: userInfo?.userId,
+                    };
+                }
                 $global.userInfo = userInfo;
                 return userInfo;
             }).catch((e) => {
@@ -42,31 +66,55 @@ const auth = {
     },
     getUserResources(DomainName) {
         if (!userResourcesPromise) {
-            userResourcesPromise = authService.GetUserResources({
-                headers: getBaseHeaders(),
-                query: {
-                    DomainName,
-                },
-            }).then((result) => {
-                const resources = result.Data.items.filter((resource) => resource.ResourceType === 'ui');
+            if (window.appInfo.hasUserCenter) {
+                userResourcesPromise = lowauthService.GetUserResources({
+                    headers: getBaseHeaders(),
+                    query: {
+                        DomainName,
+                    },
+                }).then((result) => {
+                    const resources = result.Data.items.filter((resource) => resource.ResourceType === 'ui');
 
-                // 初始化权限项
-                this._map = new Map();
-                resources.forEach((resource) => this._map.set(resource.ResourceValue, resource));
-            }).catch((e) => {
-                // 获取权限异常
-                userResourcesPromise = undefined;
-            });
+                    // 初始化权限项
+                    this._map = new Map();
+                    resources.forEach((resource) => this._map.set(resource.ResourceValue, resource));
+                }).catch((e) => {
+                    // 获取权限异常
+                    userResourcesPromise = undefined;
+                });
+            } else {
+                userResourcesPromise = authService.GetUserResources({
+                    headers: getBaseHeaders(),
+                    query: {
+                        DomainName,
+                    },
+                }).then((result) => {
+                    const resources = result.Data.items.filter((resource) => resource.ResourceType === 'ui');
+
+                    // 初始化权限项
+                    this._map = new Map();
+                    resources.forEach((resource) => this._map.set(resource.ResourceValue, resource));
+                }).catch((e) => {
+                    // 获取权限异常
+                    userResourcesPromise = undefined;
+                });
+            }
         }
         return userResourcesPromise;
     },
     logout() {
-        return authService.Logout({
-            headers: getBaseHeaders(),
-        }).then(() => {
+        if (window.appInfo.hasUserCenter) {
+            // 用户中心，去除认证和用户名信息
             cookie.erase('authorization');
             cookie.erase('username');
-        });
+        } else {
+            return authService.Logout({
+                headers: getBaseHeaders(),
+            }).then(() => {
+                cookie.erase('authorization');
+                cookie.erase('username');
+            });
+        }
     },
     /**
      * 权限服务是否初始化
