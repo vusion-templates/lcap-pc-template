d0e394ed0c4e8f3207339e3d3d1d0a295c228e48
daysai <wddpwzz36@gmail.com>
2022-02-22 14:43:01 +0800

    :fire: remove useless install vusionPackages

--- src/global/features/designer/library.js
+++ src/global/features/designer/library.js
@@ -6,8 +6,3 @@ import { installFilters, installComponents } from '@vusion/utils';
 
 installFilters(Vue, filters);
 installComponents(Vue, Components);
-
-const vusionPackages = require.context('../../../../vusion_packages', true, /\.\/([\w-]+|@[\w-]+\/[\w-]+)\/index\.js$/);
-vusionPackages.keys().forEach((key) => {
-    installComponents(Vue, vusionPackages(key));
-});
