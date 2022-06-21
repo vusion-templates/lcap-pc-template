b172e55f4ffc27c3f558b2cf7e67583c7e26cd6b
hzlingqiao <hzlingqiao@corp.netease.com>
2022-06-21 16:48:14 +0800

    feat: 修改全局逻辑调用方式

--- src/global/features/logic/install.js
+++ src/global/features/logic/install.js
@@ -1,20 +1,8 @@
+import { createService } from '@/global/features/service/create';
+
 export default {
     install(Vue, options = {}) {
         const logicsMap = Object.assign({}, options.logicsMap);
-        const keys = Object.keys(logicsMap);
-
-        Vue.prototype.$logics = {};
-        keys.forEach((key) => {
-            if (Vue.prototype.$logics[key]) {
-                throw new Error('logics repeat:' + key);
-            }
-            const logicStr = logicsMap[key];
-            /* eslint-disable no-new-func */
-            logicsMap[key] = new Function(logicStr)();
-        });
-
-        if (keys.length) {
-            Vue.prototype.$logics = Object.assign({}, Vue.prototype.$logics, logicsMap);
-        }
+        Vue.prototype.$logics = createService(logicsMap);
     },
 };
