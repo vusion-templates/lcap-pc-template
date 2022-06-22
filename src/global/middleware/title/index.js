a1db244b43889b0564e70ea5e085ff6af8ed643a
daysai <wddpwzz36@gmail.com>
2022-06-22 17:08:49 +0800

    :fire: rm "轻舟・低代码" title

--- src/global/middleware/title/index.js
+++ src/global/middleware/title/index.js
@@ -3,13 +3,12 @@ import { getComponentOption } from '../util';
 export default {
     after(appConfig) {
         return function ({ to, from, appConfig }) {
-            const appendTitle = (title) => title + ' - 轻舟・低代码';
             const metaTitle = to.matched.concat().reverse().map((item) => {
                 const componentOptions = getComponentOption(item);
                 return componentOptions?.meta?.title || item.meta?.title;
             }).filter((i) => i)[0];
             if (metaTitle) {
-                document.title = appendTitle(isFunction(metaTitle) ? metaTitle(to, from) : metaTitle);
+                document.title = isFunction(metaTitle) ? metaTitle(to, from) : metaTitle;
             }
         };
     },
