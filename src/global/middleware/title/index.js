ccfbb52d8a2b830cb13fed867e996197816a3ae5
yangxiuxiu <1974364190@qq.com>
2022-07-22 16:32:18 +0800

    :bug:title不存在时使用name代替,变量改为$页面标题

--- src/global/middleware/title/index.js
+++ src/global/middleware/title/index.js
@@ -5,14 +5,14 @@ export default {
         return function ({ to, from, appConfig }) {
             const metaTitle = to.matched.concat().reverse().map((item) => {
                 const componentOptions = getComponentOption(item);
-                return componentOptions?.meta?.title || item.meta?.title;
+                return componentOptions?.meta?.title || item.path.slice(1) || item.meta?.title;
             }).filter((i) => i)[0];
             if (metaTitle) {
                 if (isFunction(metaTitle)) {
                     document.title = metaTitle(to, from);
                 } else {
                     if (appConfig.documentTitle) {
-                        document.title = appConfig.documentTitle.replace('$页面名称', metaTitle).replace('$应用名称', appConfig.project);
+                        document.title = appConfig.documentTitle.replace('$页面标题', metaTitle).replace('$应用名称', appConfig.project);
                     } else {
                         document.title = metaTitle;
                     }
