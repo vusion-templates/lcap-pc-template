45c49579edaeea869cbf53b88a700e5e51539fb2
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-17 00:02:47 +0800

    chore: 调整init.js

--- src/init.js
+++ src/init.js
@@ -18,7 +18,36 @@ import { initRouter } from './router';
 
 window.appVue = Vue;
 
-const init = (appConfig, platformConfig, rootRoute, metaData) => {
+const traverseRoutes = (route, allowedPaths, ancestorPaths, newRoutes) => {
+    const routePath = route.path;
+    if (!Array.isArray(ancestorPaths)) {
+        ancestorPaths = [];
+    }
+    const completePath = [...ancestorPaths, routePath].join('/');
+
+    let completeRedirectPath = '';
+    const redirectPath = route.redirect;
+    if (redirectPath) {
+        completeRedirectPath = [...ancestorPaths, redirectPath].join('/');
+    }
+    let newRoute = null;
+    if (allowedPaths.includes(completePath) || allowedPaths.includes(redirectPath)) {
+        newRoute = {
+            ...route,
+        };
+        newRoutes.push(newRoute);
+    }
+    const routeChildren = route.children;
+    if (newRoute && Array.isArray(routeChildren) && routeChildren.length) {
+        newRoute.children = [];
+        for (let i = 0; i < routeChildren.length; i++) {
+            const subView = routeChildren[i];
+            traverseRoutes(subView, allowedPaths, [...ancestorPaths, routePath], newRoute.children);
+        }
+    }
+};
+
+const init = (appConfig, platformConfig, routes, metaData) => {
     window.appInfo = Object.assign(appConfig, platformConfig);
     initMiddleware(appConfig);
     installFilters(Vue, filters);
@@ -29,9 +58,19 @@ const init = (appConfig, platformConfig, rootRoute, metaData) => {
     Vue.use(installDataTypes, metaData);
     Vue.use(installUtils, metaData);
 
+    const baseResourcePaths = platformConfig.baseResourcePaths || [];
+
+    const baseRoutes = [];
+    if (Array.isArray(routes)) {
+        for (let i = 0; i < routes.length; i++) {
+            const route = routes[i];
+            traverseRoutes(route, baseResourcePaths, null, baseRoutes);
+        }
+    }
+
     const app = new Vue({
         name: 'app',
-        router: initRouter(rootRoute),
+        router: initRouter(baseRoutes),
         ...App,
     });
     app.$mount('#app');
