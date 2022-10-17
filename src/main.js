107db320e84a0404e985825d238e67b8b2638f38
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-18 01:16:25 +0800

    chore: 修改library.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import metaData from './metaData.json';
-import appConfig from './app.config';
 import platformConfig from './platform.config.json';
 import routes from './router/routes.js';
 import './library';
-
 import { init } from './init';
 
-init(appConfig, platformConfig, routes, metaData);
+init(platformConfig?.appConfig, platformConfig, routes, metaData);
