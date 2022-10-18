230ad94418b8e443f65f347c52de87c02127a084
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-18 09:39:33 +0800

    chore: 调整部分文件

--- src/apis/process/index.js
+++ src/apis/process/index.js
@@ -1,7 +1,8 @@
-import { createService } from '@/utils/create';
-import api from './api';
-import apiConfig from './api.config';
 import merge from 'lodash/merge';
+
+import { createService } from '@/utils/create';
+import apiConfig from './api.config';
+import api from './api';
 
 const service = createService(merge(api, apiConfig));
 
