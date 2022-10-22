f51a4fbb597aaebedbcaa64f5575094bb2b63bbd
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-23 02:29:42 +0800

    feat: 加上cloud-ui的别名

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import metaData from './metaData.json';
 import platformConfig from './platform.config.json';
 import { routes } from './router/routes';
 import './library';
-import * as cloudAdminDesigner from './init';
+import cloudAdminDesigner from './init';
 
 cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);
