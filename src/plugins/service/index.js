5032f2c3bbc2691587ef74b1638f6426be08c5dc
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-26 16:16:09 +0800

    fix: Bug-64712: 测试环境，权限中心添加成员，未调接口，查询不到用户 https://overmind-project.netease.com/v2/my_workbench/bugdetail/Bug-64712

--- src/plugins/service/index.js
+++ src/plugins/service/index.js
@@ -1,22 +1,20 @@
 import isPlainObject from 'lodash/isPlainObject';
 
-import { createLogicService } from '@/utils/create';
+import { createService } from '@/utils/create';
 
 export default {
     install(Vue, options = {}) {
         const services = Object.assign({}, options.servicesMap);
         const keys = Object.keys(services);
-        if (Vue.prototype.$services) {
-            keys.forEach((key) => {
-                if (Vue.prototype.$services[key]) {
-                    throw new Error('services repeat:' + key);
-                }
-                const service = services[key];
-                if (isPlainObject(service)) {
-                    services[key] = createLogicService(service);
-                }
-            });
-        }
+        keys.forEach((key) => {
+            if (Vue.prototype.$services && Vue.prototype.$services[key]) {
+                throw new Error('services repeat:' + key);
+            }
+            const service = services[key];
+            if (isPlainObject(service)) {
+                services[key] = createService(service);
+            }
+        });
         if (keys.length) {
             Vue.prototype.$services = Object.assign({}, Vue.prototype.$services, services);
         }
