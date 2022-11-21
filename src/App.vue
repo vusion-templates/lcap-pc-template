5028556f3ea85ee80f74284530aa61f2120cec28
CN\jianglinghao <jianglinghao@corp.netease.com>
2022-11-21 13:54:26 +0800

    style: appvue 样式优化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div style="height:100%">
         <s-freesass-banner v-if="isFreeSass"></s-freesass-banner>
         <router-view></router-view>
         <s-freesass-login ref="freeSassLogin"></s-freesass-login>
