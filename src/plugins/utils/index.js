842a8d56d0dadb2c617c07dcceb9d471374c57d5
Zirun <zhuzirun01@corp.netease.com>
2022-12-01 15:31:50 +0800

    增加 ListToMap

--- src/plugins/utils/index.js
+++ src/plugins/utils/index.js
@@ -316,40 +316,40 @@ export const utils = {
         return 0;
     },
     MapValues(map) {
-        if (isObject(map)) {
-            if ('values' in Object) {
-                return Object.values(map);
-            } else {
-                const res = [];
-                for (const key in map) {
-                    if (Object.hasOwnProperty.call(map, key)) {
-                        res.push(map[key]);
-                    }
+        if (!isObject(map)) {
+            return [];
+        }
+        if ('values' in Object) {
+            return Object.values(map);
+        } else {
+            const res = [];
+            for (const key in map) {
+                if (Object.hasOwnProperty.call(map, key)) {
+                    res.push(map[key]);
                 }
-                return res;
             }
+            return res;
         }
-        return [];
     },
-    MapFilter(map, predicate) {
-        if (!isObject(map) || typeof predicate != 'function') {
-            return null;
-        }
-        const res = new Map();
-        for (const key in map) {
-            if (Object.hasOwnProperty.call(map, key) && predicate.call(this, key, map[key])) {
-                res.push(key, map[key]);
-            }
-        }
-        return res;
-    },
-    MapTransform(map, trans) {
-        if (!isObject(map) || typeof predicate != 'function') {
+    MapFilter(map, by) {
+        if (!isObject(map) || !(typeof predicate === 'function')) {
             return null;
         }
         const res = new Map();
         for (const [k, v] of map) {
-            res.push(trans(k, v));
+            if (by(k, v)) {
+                res.set(k, v);
+            }
+        }
+        return res;
+    },
+    MapTransform(map, by) {
+        if (!isObject(map) || typeof predicate !== 'function') {
+            return null;
+        }
+        const res = new Map();
+        for (const [k, v] of map) {
+            res.set(...by(k, v));
         }
         return res;
     },
@@ -372,9 +372,17 @@ export const utils = {
     },
     // let mmm2 = new Map([['a', 1], ['b', 1], ['c', 1], ['d', 4]]);
     // let mmm3 = MapDistinctBy(mmm2, (k, v) => v); // [ Map(2) { 'a' => 1, 'd' => 4 } ]
-    ListToMap(list) {
-        console.log("ListToMap is not implemented yet.");
-        console.assert(false);
+    ListToMap(arr, toKey, toValue) {
+        if (typeof arr != 'object' || typeof toKey != 'function' || typeof toValue != 'function') {
+            return null;
+        }
+        const res = new Map();
+        arr.forEach(e => {
+            if (toKey(e)) {
+                res.set(toKey(e), toValue(e));
+            }
+        });
+        return res;
     },
     CurrDate() {
         return new Date().toJSON().replace(/T.+?Z/, '');
