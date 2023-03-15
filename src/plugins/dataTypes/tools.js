5e6afcee26f343a60dca51b36cc2960e9dd62c69
hzlingqiao <hzlingqiao@corp.netease.com>
2023-03-15 14:10:46 +0800

    throw Error

--- src/plugins/dataTypes/tools.js
+++ src/plugins/dataTypes/tools.js
@@ -656,5 +656,5 @@ export const fromString = (variable, typeKey) => {
             return JSON.parse(lowerVar);
         }
     }
-    return new Error(`${typeName}格式不正确`);
+    throw Error(`${typeName}格式不正确`);
 };
