fa4683d13b85381222dba4020e0e052c97bd0211
hzlingqiao <hzlingqiao@corp.netease.com>
2023-03-21 11:14:21 +0800

    feat: 加上判等方法

--- src/plugins/dataTypes/index.js
+++ src/plugins/dataTypes/index.js
@@ -309,6 +309,47 @@ export default {
             enumsMap[enumKey] = createEnum(enumsMap[enumKey] || {});
         });
 
+        // 判断两个对象是否相等，不需要引用完全一致
+        Vue.prototype.$isLooseEqualFn = (obj1, obj2, cache = new Map()) => {
+            // 检查对象是否相同
+            if (obj1 === obj2) {
+                return true;
+            }
+            // 对象是否已经比较过，解决循环依赖的问题
+            if (cache.has(obj1) && cache.get(obj1) === obj2) {
+                return true;
+            }
+            // 判断类型相等
+            if (typeof obj1 !== typeof obj2) {
+                return false;
+            }
+            // 判断数组长度或者对象属性数量一致
+            const keys1 = Object.keys(obj1);
+            const keys2 = Object.keys(obj2);
+            if (keys1.length !== keys2.length) {
+                return false;
+            }
+            // 加入缓存中
+            cache.set(obj1, obj2);
+            // 比较属性中的每个值是否一致
+            for (const key of keys1) {
+                const val1 = obj1[key];
+                const val2 = obj2[key];
+                // 递归
+                if (typeof val1 === 'object' && typeof val2 === 'object') {
+                    if (!this.$isEqualFn(val1, val2, cache)) {
+                        return false;
+                    }
+                } else {
+                    // 判断非对象的值是否一致
+                    if (val1 !== val2) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        };
+
         Vue.prototype.$enums = (key, value) => {
             if (!key || !value)
                 return '';
