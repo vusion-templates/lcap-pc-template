4ec6cbf66efc06b7d6d967d7d5a3d30317f9c78c
zanningwang <wangzanning@me.com>
2023-04-06 20:02:59 +0800

    fix: 修复同页面锚点跳转带参数时，第一次失效的问题

--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -32,7 +32,7 @@ export default {
                 else {
                     // 处理同页面锚点跳转无效的问题
                     const beforeHashUrl = url.slice(0, url.indexOf('#'));
-                    if (url.indexOf('#') !== -1 && beforeHashUrl === (location.pathname + location.search)) {
+                    if (url.indexOf('#') !== -1 && beforeHashUrl.indexOf(location.pathname) !== -1) {
                         const hash = url.slice(url.indexOf('#'))?.replace('#', '');
                         if (document.getElementById(hash)) {
                             document.getElementById(hash).scrollIntoView();
