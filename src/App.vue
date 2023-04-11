84a4cfb4ecd9c41631578f7515992bee809a2a1a
CN\jianglinghao <jianglinghao@corp.netease.com>
2023-04-11 19:03:40 +0800

    feat: 支持制品 & 数帆打通登录

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,27 @@
     <div style="height:100%">
         <s-freesass-banner v-if="isFreeSass"></s-freesass-banner>
         <router-view></router-view>
-        <s-freesass-login ref="freeSassLogin"></s-freesass-login>
+        <s-freesass-login @afterShufanLogin="afterShufanLogin" ref="freeSassLogin"></s-freesass-login>
+        <s-freesass-transfer v-if="isFreeSass&&loginFinished" ref="freesassTransfer"></s-freesass-transfer>
     </div>
 </template>
 
 <script>
 import SFreesassLogin from '@/components/s-freesass-login';
+import SFreesassTransfer from '@/components/s-freesass-transfer';
 import SFreesassBanner from '@/components/s-freesass-banner';
 import auth from '@/apis/auth';
-
+const serviceMap = {
+    // generateSfToken: 'http://sfsso.lcap.hatest.163yun.com/api/generateSfToken',
+    checkSfToken: 'http://sfsso.lcap.hatest.163yun.com/api/checkSfToken',
+};
 export default {
-    components: { SFreesassLogin, SFreesassBanner },
+    components: { SFreesassLogin, SFreesassBanner, SFreesassTransfer },
+    data() {
+        return {
+            loginFinished: false,
+        };
+    },
     computed: {
         isSharePage() {
             const neteaseStrList = 'lcap.qz.163yun'.split('.');
@@ -25,14 +35,33 @@ export default {
     async mounted() {
         if (this.isSharePage && this.isFreeSass) {
             try {
-                await auth.CheckExtendToken({ config: {
-                    noErrorTip: true,
-                } });
+                // 校验接口
+                const res = await fetch(serviceMap.checkSfToken, {
+                    method: 'POST',
+                    mode: 'cors',
+                    credentials: 'include',
+                });
+                const data = await res.json();
+                // data.Code  === 200
+                if (data?.Data === true) {
+                    // 制品有sf_token 什么都不做
+                } else {
+                    this.$refs.freeSassLogin.open();
+                }
+                // 删除此处 根据后端接口控制
+                // await auth.CheckExtendToken({ config: {
+                //     noErrorTip: true,
+                // } });
             } catch (error) {
                 console.error('CheckExtendToken: ', error);
                 this.$refs.freeSassLogin.open();
             }
         }
+    },
+    methods: {
+        afterShufanLogin() {
+            this.loginFinished = true;
+        },
     },
 };
 
