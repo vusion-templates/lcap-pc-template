25ba97ab37e81dacf7e5f479693c3e0b74e091d7
CN\jianglinghao <jianglinghao@corp.netease.com>
2023-05-10 14:30:41 +0800

    feat: 适配codewave域名

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,11 @@ import SFreesassTransfer from '@/components/s-freesass-transfer';
 import SFreesassBanner from '@/components/s-freesass-banner';
 import auth from '@/apis/auth';
 
+const newDomain = location.host.split('.').includes('163');
 const serviceMap = {
     // generateSfToken: 'http://sfsso.community1.lcap.qz.163yun.com/api/generateSfToken',
-    checkSfToken: 'http://sfsso.community1.lcap.qz.163yun.com/api/checkSfToken',
+    checkSfToken: `${location.protocol}//sfsso.community1.lcap.qz.163yun.com/api/checkSfToken`,
+    checkSfTokenNew: `${location.protocol}//sfsso-community1.app.codewave.163.com/api/checkSfToken`,
 };
 export default {
     components: { SFreesassLogin, SFreesassBanner, SFreesassTransfer },
@@ -26,7 +28,9 @@ export default {
     },
     computed: {
         isSharePage() {
-            const neteaseStrList = 'lcap.qz.163yun'.split('.');
+            let str = 'lcap.qz.163yun';
+            if (newDomain) { str = 'app.codewave.163'; }
+            const neteaseStrList = str.split('.');
             return neteaseStrList.some((it) => location.host.includes(it));
         },
         isFreeSass() {
@@ -36,8 +40,10 @@ export default {
     async mounted() {
         if (this.isSharePage && +window.appInfo?.tenantType === 1) {
             try {
+                let url = serviceMap.checkSfToken;
+                if (newDomain) { url = serviceMap.checkSfTokenNew; }
                 // 校验接口
-                const res = await fetch(serviceMap.checkSfToken, {
+                const res = await fetch(url, {
                     method: 'POST',
                     mode: 'cors',
                     credentials: 'include',
