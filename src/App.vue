1c3244b58b478b6f06b648d23684f083fd792dfe
CN\jianglinghao <jianglinghao@corp.netease.com>
2023-05-10 14:30:41 +0800

    feat: 适配codewave域名

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,19 @@ import SFreesassLogin from '@/components/s-freesass-login';
 import SFreesassBanner from '@/components/s-freesass-banner';
 import auth from '@/apis/auth';
 
+const newDomain = location.host.split('.').includes('163');
+const serviceMap = {
+    // generateSfToken: 'http://sfsso.community1.lcap.qz.163yun.com/api/generateSfToken',
+    checkSfToken: `${location.protocol}//sfsso.community1.lcap.qz.163yun.com/api/checkSfToken`,
+    checkSfTokenNew: `${location.protocol}//sfsso-community1.app.codewave.163.com/api/checkSfToken`,
+};
 export default {
     components: { SFreesassLogin, SFreesassBanner },
     computed: {
         isSharePage() {
-            const neteaseStrList = 'lcap.qz.163yun'.split('.');
+            let str = 'lcap.qz.163yun';
+            if (newDomain) { str = 'app.codewave.163'; }
+            const neteaseStrList = str.split('.');
             return neteaseStrList.some((it) => location.host.includes(it));
         },
         isFreeSass() {
@@ -25,9 +33,25 @@ export default {
     async mounted() {
         if (this.isSharePage && +window.appInfo?.tenantType === 1) {
             try {
-                await auth.CheckExtendToken({ config: {
-                    noErrorTip: true,
-                } });
+                let url = serviceMap.checkSfToken;
+                if (newDomain) { url = serviceMap.checkSfTokenNew; }
+                // 校验接口
+                const res = await fetch(url, {
+                    method: 'POST',
+                    mode: 'cors',
+                    credentials: 'include',
+                });
+                const data = await res.json();
+                // data.Code  === 200
+                if (data?.Data === true) {
+                    // 制品有sf_token 什么都不做
+                } else {
+                    this.$refs.freeSassLogin.open();
+                }
+                // 删除此处 根据后端接口控制
+                // await auth.CheckExtendToken({ config: {
+                //     noErrorTip: true,
+                // } });
             } catch (error) {
                 console.error('CheckExtendToken: ', error);
                 this.$refs.freeSassLogin.open();
