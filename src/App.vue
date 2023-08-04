c7cd3895a54688bed5ea1a7f942f6bb8439e3711
daysai <wddpwzz36@gmail.com>
2023-08-04 19:14:25 +0800

    :fire: Rm useless code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
     <div style="height:100%">
-        <s-freesass-banner v-if="isFreeSass"></s-freesass-banner>
         <router-view></router-view>
         <s-freesass-login @afterShufanLogin="afterShufanLogin" ref="freeSassLogin"></s-freesass-login>
-        <s-freesass-transfer v-if="isPersonSass&&loginFinished" ref="freesassTransfer"></s-freesass-transfer>
+        <s-freesass-transfer v-if="isPersonSass && loginFinished" ref="freesassTransfer"></s-freesass-transfer>
     </div>
 </template>
 
 <script>
 import SFreesassLogin from '@/components/s-freesass-login';
 import SFreesassTransfer from '@/components/s-freesass-transfer';
-import SFreesassBanner from '@/components/s-freesass-banner';
 import { localCacheVariableMixin } from '@/mixins/localCacheVariableMixin';
 
 const newDomain = location.host.split('.').includes('163');
@@ -19,7 +17,7 @@ const serviceMap = {
     checkSfTokenNew: `${location.protocol}//sfsso-community1.app.codewave.163.com/api/checkSfToken`,
 };
 export default {
-    components: { SFreesassLogin, SFreesassBanner, SFreesassTransfer },
+    components: { SFreesassLogin, SFreesassTransfer },
     mixins: [localCacheVariableMixin],
     data() {
         return {
@@ -29,22 +27,23 @@ export default {
     computed: {
         isSharePage() {
             let str = 'lcap.qz.163yun';
-            if (newDomain) { str = 'app.codewave.163'; }
+            if (newDomain) {
+                str = 'app.codewave.163';
+            }
             const neteaseStrList = str.split('.');
             return neteaseStrList.some((it) => location.host.includes(it));
         },
         isPersonSass() {
             return +window.appInfo?.tenantType === 1;
         },
-        isFreeSass() {
-            return +window.appInfo?.tenantType === 1 && +window.appInfo?.tenantLevel === 0;
-        },
     },
     async mounted() {
         if (this.isSharePage && +window.appInfo?.tenantType === 1) {
             try {
                 let url = serviceMap.checkSfToken;
-                if (newDomain) { url = serviceMap.checkSfTokenNew; }
+                if (newDomain) {
+                    url = serviceMap.checkSfTokenNew;
+                }
                 // 校验接口
                 const res = await fetch(url, {
                     method: 'POST',
@@ -74,5 +73,4 @@ export default {
         },
     },
 };
-
 </script>
