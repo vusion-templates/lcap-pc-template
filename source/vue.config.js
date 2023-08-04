11e631ca837a9be286bfff48abe3d3f4e3526815
daysai <wddpwzz36@gmail.com>
2023-08-04 20:57:05 +0800

    :fire: Fix export source code

--- source/vue.config.js
+++ source/vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
     configureWebpack: {
         resolve: {
             alias: {
-                'cloud-ui.vusion$': path.resolve(__dirname, 'node_modules/cloud-ui.vusion/dist-raw/index.js'),
-                'cloud-ui.vusion.css$': path.resolve(__dirname, 'node_modules/cloud-ui.vusion/dist-raw/index.css'),
+                'cloud-ui.vusion$': path.resolve(__dirname, 'node_modules/cloud-ui.vusion/dist-theme/index.js'),
+                'cloud-ui.vusion.css$': path.resolve(__dirname, 'node_modules/cloud-ui.vusion/dist-theme/index.css'),
             },
         }
     },
