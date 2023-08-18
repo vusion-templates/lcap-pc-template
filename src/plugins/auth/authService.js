e0fd644e9b6132d8e80d45d0e66ec3a49af8227a
周勋 <zhouxun03@corp.netease.com>
2023-08-18 17:33:13 +0800

    feat: resourceType 兼容'ui'

--- src/plugins/auth/authService.js
+++ src/plugins/auth/authService.js
@@ -78,7 +78,7 @@ export default {
                 // 初始化权限项
                 this._map = new Map();
                 if (Array.isArray(result)) {
-                    resources = result.filter((resource) => resource?.resourceType === pageResourceType);
+                    resources = result.filter((resource) => resource?.resourceType === pageResourceType || resource?.resourceType === 'ui');
                     resources.forEach((resource) => this._map.set(resource.resourceValue, resource));
                 }
                 return resources;
@@ -96,7 +96,7 @@ export default {
             }).then((res) => {
                 this._map = new Map();
                 const resources = res.Data.items.reduce((acc, { ResourceType, ResourceValue, ...item }) => {
-                    if (ResourceType === pageResourceType) {
+                    if (ResourceType === pageResourceType || ResourceType === 'ui') {
                         acc.push({ ...item, ResourceType, ResourceValue, resourceType: ResourceType, resourceValue: ResourceValue }); // 兼容大小写写法，留存大写，避免影响其他隐藏逻辑
                     }
                     return acc;
