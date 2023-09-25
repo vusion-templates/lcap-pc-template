038075a189d71167fe4e50c4b82c0c34d869942e
hzlingqiao <hzlingqiao@corp.netease.com>
2023-09-25 10:36:56 +0800

    chore: toString不使用科学技术法

--- src/plugins/dataTypes/tools.js
+++ src/plugins/dataTypes/tools.js
@@ -505,7 +505,8 @@ export const toString = (variable, typeKey, tabSize = 0, collection = new Set())
             const maxLen = 8;
             if (count >= maxLen) {
                 // 去掉+是为了跟后端保持统一
-                str = (+variable)?.toExponential?.().replace?.('e+', 'e');
+                // str = (+variable)?.toExponential?.().replace?.('e+', 'e');
+                str = variable;
             }
         }
         // 日期处理
