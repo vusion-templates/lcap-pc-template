63e67f4048cbd6e722ceaf1bebea6f9e33d9fc11
姜令浩 <jianglinghao@corp.netease.com>
2023-12-25 09:52:36 +0800

    feat: 端事件优化：preRequest

--- src/utils/create/index.js
+++ src/utils/create/index.js
@@ -196,7 +196,7 @@ export const createLogicService = function createLogicService(apiSchemaList, ser
     serviceConfig = fixServiceConfig;
     const newApiSchemaMap = adjustPathWithSysPrefixPath(apiSchemaList);
     if (window.preRequest) {
-        service.preConfig.set('preRequest', (requestInfo, preData) => {
+        let resolve  = (requestInfo, preData) => {
             const HttpRequest = {
                 requestURI: requestInfo.url.path,
                 remoteIp: '',
@@ -205,10 +205,13 @@ export const createLogicService = function createLogicService(apiSchemaList, ser
                 headers: requestInfo.url.headers,
                 querys: JSON.stringify(requestInfo.url.query),
                 cookies: foramtCookie(document.cookie),
+                requestInfo
             };
 
-            window.preRequest && window.preRequest(HttpRequest, preData);
-        });
+            return  window.preRequest && window.preRequest(HttpRequest, preData);
+        }
+
+        service.preConfig.set('preRequest',   {resolve});
         serviceConfig.config.preRequest = true;
     }
     if (window.postRequest) {
