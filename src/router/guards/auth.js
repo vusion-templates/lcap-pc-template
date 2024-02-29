54bbb890e567dabfe034247faf8978394f324bb3
周勋 <zhouxun03@corp.netease.com>
2024-02-29 15:25:40 +0800

    Bug-2816596461515264: compatible multi layer url

--- src/router/guards/auth.js
+++ src/router/guards/auth.js
@@ -17,6 +17,18 @@ const ROOT_PATH = '/';
 
 const getParentPath = (path) => path === ROOT_PATH ? null : path.substring(0, path.lastIndexOf('/')) || ROOT_PATH;
 
+function generatePaths(str) {
+    let parts = str.split('/');
+    let paths = [];
+  
+    for (let i = 0; i < parts.length; i++) {
+      let path = parts.slice(0, i + 1).join('/');
+      path && paths.push([path, 1]);
+    }
+  
+    return paths;
+  }
+
 /**
  * 过滤无权限页面（X2.22_0629调整），如子页面绑定了角色父页面未绑定，则子页面无法访问。
  * 更多边界情况参考用例: tests\unit\global\routes\route.spec.js
@@ -26,11 +38,13 @@ export function filterAuthResources(resources) {
     if (!Array.isArray(resources) || !resources.length)
         return [];
 
+     const bases = generatePaths(getBasePath());
+
     const validPaths = resources.reduce((map, item) => {
         map.set(item.resourceValue, 1);
         return map;
-    }, new Map([[ROOT_PATH, 1], [getBasePath(), 1]])); // 需注意，路由起始都具备basePath（PC&H5都有不固定起始路由）
-
+    }, new Map([[ROOT_PATH, 1], ...bases])); // 需注意，路由起始都具备basePath（PC&H5都有不固定起始路由）
+    
     const isValidPath = (path) => {
         let parentPath = getParentPath(path);
         while (parentPath && validPaths.has(parentPath))
